362928079ad8cd29d9af1b297227c0bd8285442c
Rudresh Patre <rudreshpatre@Rudreshs-MacBook-Pro.local>
2023-03-26 22:39:23 +0530

    Use Add operation of Calculator

--- calculator-using-github-co-pilot/Calculator/Program.cs
+++ calculator-using-github-co-pilot/Calculator/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Calculator!, build using github co-pilot");
+
+            var calculator = new Calculator.Domain.Calculator();
+            Console.WriteLine("Lets add 1 and 2");
+            var result = calculator.Add(1, 2);
+            Console.WriteLine($"The result is {result}");
+        }
+    }
+}
